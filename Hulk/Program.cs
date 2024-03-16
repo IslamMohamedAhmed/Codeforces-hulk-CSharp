017fe35823be31947695d12f1c8b711b8f0dca5f
unknown <islammohamedahmed49@gmail.com>
2024-03-16 18:51:28 +0200

    hulk

---
+++ Hulk/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+int n;
+string str = "it";
+n = ToInt32(ReadLine());
+for(int i = 0; i < n; i++)
+{
+    if (i == n - 1)
+    {
+        str = "it";
+    }
+    else
+    {
+        str = "that ";
+    }
+    if (i % 2 == 0)
+    {
+        Write($"I hate {str}");
+    }
+    else
+    {
+        Write($"I love {str}");
+    }
+}
+WriteLine();
